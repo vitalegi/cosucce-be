17a847483d2507341ef4de7418b2f8286fbb92df
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2026-04-07 19:12:46 +0200

    init repository

---
+++ src/main/java/it/vitalegi/cosucce/CosucceApplication.java
@@ -0,0 +1,13 @@
+package it.vitalegi.cosucce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosucceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosucceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/vitalegi/cosucce/CosucceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vitalegi.cosucce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosucceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
